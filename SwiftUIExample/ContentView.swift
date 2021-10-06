0deb250adc56ac84c58f4a580a454bfb07084e56
Yosuke <ihatenonoildressing@gmail.com>
2021-10-06 20:38:24 +0900

    added ProjectFile

---
+++ SwiftUIExample/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  SwiftUIExample
+//
+//  Created by yosuke.nakayama on 2021/10/06.
+//  Copyright © 2021 Yummypets. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
